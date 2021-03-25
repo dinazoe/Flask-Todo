0577f51d27442d0caaeb9caf40eee5c69cce7ad3
Dmitriy Kapitanov <dinazouraus@gmail.com>
2021-03-25 14:46:01 -0400

    Adding base.html and index.html logic

--- app.py
+++ app.py
@@ -10,11 +10,10 @@ db = SQLAlchemy(app)
 class Todo(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200), nullable=False)
-    completed = db.Column(db.Integer, default=0)
     date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
     def __repr__(self):
-        return '<Task %r>' % self.id 
+        return '<Task %r>' % self.id
 
 
 
